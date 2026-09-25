6f4e4cb2f123621dfb5230a72add5adae8ea7279
Katttzk <catiadorotea92@gmail.com>
2026-09-25 20:23:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Cola aqui o "ID de cliente" OAuth que criaste na Google Cloud Console (ver LEIA-ME.md).
 // Sem isto a app funciona na mesma, mas só guarda os dados neste aparelho.
 window.AXO_CONFIG = {
-  'googleClientId: 610284523747-o48lqjsfdajtcr67tiffrg6oultpq8na.apps.googleusercontent.com'
+  googleClientId: '610284523747-o48lqjsfdajtcr67tiffrg6oultpq8na.apps.googleusercontent.com'
 };
